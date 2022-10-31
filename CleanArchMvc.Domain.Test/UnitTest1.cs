c38c7719a879e1a54ac8edc8f9c378a0735f4efd
reginaldo.marinho <reginaldomarinho2016@gmail.com>
2022-10-31 16:59:04 -0300

    commit

---
+++ CleanArchMvc.Domain.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CleanArchMvc.Domain.Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Action action () => new Category();
+    }
+}
